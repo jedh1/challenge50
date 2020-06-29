ba336416536c3e0999056ba247433fd7a68082c9
Jed <jedhcl@gmail.com>
2020-06-29 14:31:58 -0700

    changed marriott search to headless. Removed django-background-task and installed django-background-tasks. Updated requirements.txt

--- hotelm/marriott.py
+++ hotelm/marriott.py
@@ -12,25 +12,24 @@ from selenium.webdriver.support.wait import WebDriverWait
 from challenge.settings import EMAIL_HOST_USER
 
 def driver_test():
-    print('driver_test start')
-    options = Options()
-    options.add_argument('--window-size=1400,800')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-extensions')
+    # print('driver_test start')
+    # options = Options()
+    # options.add_argument('--window-size=1400,800')
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--disable-extensions')
     # options.add_argument('--headless')
-    #load Chrome driver
-    driver = webdriver.Chrome(executable_path="C:\misc\CS\Projects\challenge\hotelm\drivers\chromedriver.exe", chrome_options=options)
+    # driver = webdriver.Chrome(executable_path="C:\misc\CS\Projects\challenge\hotelm\drivers\chromedriver.exe", chrome_options=options)
 
     # Selenium for herokuapp
     # https://www.andressevilla.com/running-chromedriver-with-python-selenium-on-heroku/
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument('--window-size=1920,1080')
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument("--no-sandbox")
-    # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("https://www.marriott.com/search/default.mi")
     wait = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.NAME, 'destinationAddress.destination')))
     print('driver success')
