e23756a7d08a4dd3ce0c2c8c7d178110877984c1
Jed <jedhcl@gmail.com>
2020-06-30 13:33:29 -0700

    changed settings.py staticfiles root

--- challenge/settings.py
+++ challenge/settings.py
@@ -143,12 +143,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# Static files (CSS, JavaScript, Images)--------------------------
+# https://docs.djangoproject.com/en/3.0/howto/static-files/-------
 
 STATIC_URL = '/static/'
 #location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #
